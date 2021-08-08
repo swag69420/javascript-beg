476a3d3e930a3aec1aaf7b0b826a0bc7b1aa43b3
Nick Brady <dankest.memer@yahoo.com>
2021-08-07 17:37:22 -0700

    app.js

---
+++ 2DCanvasGame/app.js
@@ -0,0 +1,12 @@
+//Create variables to reference and store canvas
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let ballRadius = 10;
+let x = canvas.wdith/2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = 2;
+//create the paddle
+let paddleHeight = 12;
+let paddleWidth = 72;
+//
